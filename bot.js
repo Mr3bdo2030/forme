5db5c9cc059eee7e7bd8c4aa1f6e2a22b54986d5
Mr3bdo2030 <47391388+Mr3bdo2030@users.noreply.github.com>
2019-02-15 13:37:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "#";
 var adminprefix = '#'
 
 
-const developers = ["487335775814287360","526035894071984132"]
+const developers = ["487335775814287360","526035894071984132","538354545810735109"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
